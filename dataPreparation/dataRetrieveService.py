6432f62e43cb3769d60155ec9e704088617d42ca
wangfeng2016 <feng.wang.wf@outlook.com>
2016-01-12 08:52:33 +0800

    added dataRetrieveService.py in folder dataPreparation

---
+++ dataPreparation/dataRetrieveService.py
@@ -0,0 +1,27 @@
+#-------------------------------------------------------------
+# Name:       dataRetrieveService.py
+# Purpose:    class provide APIs to retrieve stock data from data source
+# Author:     wang feng / Han Jie / add your name here
+# Created:    10/01/2016
+# Copyright:  
+# Python Version:   2.6
+#-------------------------------------------------------------
+
+import os
+import sys
+import requests;
+import re;
+
+class dataRetrieveService():
+	def getStockCodes():
+		print "not implemented"
+
+	def getStockNameByCode(code):
+		print "not implemented"
+
+	def getQuoteByCode(code):
+		print "not implemented"
+
+	def getHistoryQuoteByCode(code):
+		print "not implemented"
+
