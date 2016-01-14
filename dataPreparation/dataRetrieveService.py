bf5ccdda5c1e69c8e2f0484f66c8ea5a6b17af96
hanjie <hanjie@sina.com>
2016-01-14 21:11:06 +0800

    minor

--- dataPreparation/dataRetrieveService.py
+++ dataPreparation/dataRetrieveService.py
@@ -1,12 +1,13 @@
-#-------------------------------------------------------------
-# Name:       dataRetrieveService.py
-# Purpose:    class provide APIs to retrieve stock data from data source
-# Author:     wang feng / Han Jie / add your name here
-# Created:    10/01/2016
-# Copyright:  
-# Python Version:   2.6
-#-------------------------------------------------------------
-
+'''
+-------------------------------------------------------------
+ Name:       dataRetrieveService.py
+ Purpose:    class provide APIs to retrieve stock data from data source
+ Author:     wang feng / Han Jie / add your name here
+ Created:    10/01/2016
+ Copyright:  
+ Python Version:   2.6
+-------------------------------------------------------------
+'''
 import os
 import sys
 import requests;
